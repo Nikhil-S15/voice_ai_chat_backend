1ef043402161f99492d4134bb6e240cac7e640b8
Malavika MU <mumalavika@gmail.com>
2025-08-23 22:14:37 +0530

    changes5

--- config/database.js
+++ config/database.js
@@ -1,28 +1,4 @@
-// const { Sequelize } = require("sequelize");
-// require("dotenv").config();
 
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASS,
-//   {
-//     host: process.env.DB_HOST,
-//     dialect: "postgres",
-//     logging: false,
-//   }
-// );
-
-// // Test the connection
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log("✅ PostgreSQL connection has been established successfully.");
-//   })
-//   .catch((err) => {
-//     console.error("❌ Unable to connect to the PostgreSQL database:", err);
-//   });
-
-// module.exports = sequelize;
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
@@ -51,7 +27,7 @@ sequelize
     console.log("✅ PostgreSQL connection has been established successfully.");
   })
   .catch((err) => {
-    console.error("❌ Unable to connect to the PostgreSQL database:", err);
+    console.error("❌ Unable to connect to the PostgreSQL databases:", err);
   });
 
 module.exports = sequelize;
