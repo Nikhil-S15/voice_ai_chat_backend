f26058f3a34e120e7dc5db0de33cb94d7b2e718d
Malavika MU <mumalavika@gmail.com>
2025-08-23 22:02:36 +0530

    changes2

--- config/database.js
+++ config/database.js
@@ -32,7 +32,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,    // your DB password
   {
     host: process.env.DB_HOST,   // e.g. dpg-d2klbm15pdvs739ofs50-a.oregon-postgres.render.com
-    port: process.env.DB_PORT || 5432,
+    port: process.env.DB_PORT ,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
